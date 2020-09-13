b35af566091d3976bb0f135ec131d3f46513be83
Demis Bellot <demis.bellot@gmail.com>
2020-09-13 22:52:26 +0800

    Use default UseSameSiteCookies

--- MyApp/Startup.cs
+++ MyApp/Startup.cs
@@ -51,7 +51,6 @@ namespace MyApp
 
             SetConfig(new HostConfig
             {
-                UseSameSiteCookies = true,
                 AddRedirectParamsToQueryString = true,
                 DebugMode = AppSettings.Get(nameof(HostConfig.DebugMode), HostingEnvironment.IsDevelopment()),
             });
