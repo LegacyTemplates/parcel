be74788018213bfa0583a487be9e88a745beb93f
Demis Bellot <demis.bellot@gmail.com>
2021-11-24 14:53:44 +0800

    upgrade to net6.0

--- MyApp.ServiceInterface/MyServices.cs
+++ MyApp.ServiceInterface/MyServices.cs
@@ -8,18 +8,8 @@ using MyApp.ServiceModel;
 
 namespace MyApp.ServiceInterface
 {
-    [Exclude(Feature.Metadata)]
-    [FallbackRoute("/{PathInfo*}", Matches="AcceptsHtml")]
-    public class FallbackForClientRoutes
-    {
-        public string PathInfo { get; set; }
-    }
-
     public class MyServices : Service
     {
-        //Return index.html for unmatched requests so routing is handled on client
-        public object Any(FallbackForClientRoutes request) => Request.GetPageResult("/");
-
         public object Any(Hello request)
         {
             return new HelloResponse { Result = $"Hello, {request.Name}!" };
